db7e7d9ac143a5a38a51a77101abb56f35752c09
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-01-15 10:23:17 +0100

    Attempt to fix memory issues.
    Extract features before joining the two data sets. This reduces the
    memory requirements significantly.

--- src/data/spark/extract_features_from_joined_data.py
+++ src/data/spark/extract_features_from_joined_data.py
@@ -1,5 +1,5 @@
 # Command to run this script on the CTIT cluster:
-# $ spark-submit --master yarn --deploy-mode cluster src/data/spark/extract_features_from_joined_data.py
+# $ spark-submit --master yarn --deploy-mode cluster --driver-memory 4g --executor-memory 2g src/data/spark/extract_features_from_joined_data.py
 
 from pyspark import SparkContext
 import json
@@ -33,22 +33,24 @@ def tokinise_records(line):
     domain_name = data['domain']
     date_str = data['date']
     records = data['records']
+    # Create a list
+    output_list = [len(records)]
+    records_str = ';'.join(records)
+    for rtype in ['SOA', 'NS', 'A', 'AAAA', 'CNAME', 'MX', 'TXT', 'SPF', 'PTR', 'DNSKEY', 'DS', 'RRSIG', 'CAA', 'NSEC']:
+        count = records_str.count(' %s ' % rtype)
+        output_list.append(count)
     key = '%s-%s' % (domain_name, date_str)
-    return key, records
+    return key, output_list
 
 
-def extract_features(input_tup):
+def to_csv_line(input_tup):
     k, v = input_tup
-    alexa_rank, records = v
+    alexa_rank, counts = v
     dash_idx = k.rfind('-')
     domain_name = k[:dash_idx]
     date_str = k[dash_idx+1:]
     # Generate the output list
-    output_list = [domain_name, date_str, str(alexa_rank), str(len(records))]
-    records_str = ';'.join(records)
-    for rtype in ['SOA', 'NS', 'A', 'AAAA', 'CNAME', 'MX', 'TXT', 'SPF', 'PTR', 'DNSKEY', 'DS', 'RRSIG', 'CAA', 'NSEC']:
-        count = records_str.count(' %s ' % rtype)
-        output_list.append(str(count))
+    output_list = [domain_name, date_str, str(alexa_rank)] + list(map(str, counts))
     return ','.join(output_list)
 
 
@@ -61,7 +63,7 @@ records_rdd = sc.textFile(other_path)
 records_rdd = records_rdd.map(tokinise_records)
 
 joined_rdd = rankings_rdd.join(records_rdd)
-joined_rdd = joined_rdd.map(extract_features)
+joined_rdd = joined_rdd.map(to_csv_line)
 
 out_path = '/user/s1962523/openintel-alexa1m-ranking-2016-11'
 joined_rdd.saveAsTextFile(path=out_path,
