b8bf1ec6f3c964d5799bdfb66cfeb18eda6fb0cd
Omar Ali Sheikh <sheikhomar@gmail.com>
2018-01-18 15:54:46 +0100

    Filter out IDNs.

--- src/data/spark/compute_domain_entropy.py
+++ src/data/spark/compute_domain_entropy.py
@@ -2,7 +2,6 @@
 # $ spark-submit --master yarn --deploy-mode cluster src/data/spark/compute_domain_entropy.py
 
 from pyspark import SparkContext
-import re
 import math
 
 sc = SparkContext(appName="Compute Domain Name Entropy")
@@ -13,7 +12,6 @@ sc.setLogLevel("ERROR")
 # Source: https://answers.splunk.com/answers/13636/calculate-entropy-just-entropy-not-change-in-entropy-like-associate.html
 def entropy(string):
     """Calculates the Shannon entropy of a string"""
-
     prob = [float(string.count(c)) / len(string) for c in dict.fromkeys(list(string))]
     entropy = - sum([p * math.log(p) / math.log(2.0) for p in prob])
     return entropy
@@ -29,16 +27,17 @@ def entropy_ideal(length):
 
 def to_tuple(line):
     fields = line.split(',')
+    avg_ranking = int(float(fields[4]))
     domain_name = fields[0].lower()
     sld = domain_name[:domain_name.rfind('.')]
     etp = entropy(sld)
     etp_ideal = entropy_ideal(len(sld))
-    return domain_name, etp, etp_ideal
+    return domain_name, etp, etp_ideal, avg_ranking
 
 
 rdd = sc.textFile('/user/s1962523/agg-alexa/part-*')
 rdd = rdd.map(to_tuple)
-rdd = rdd.sortBy(lambda t: t[1])
+rdd = rdd.filter(lambda t: not t[0].startswith('xn--'))
 rdd = rdd.map(lambda t: ','.join(map(str, t)))
 rdd = rdd.coalesce(1)
 rdd.saveAsTextFile(path='/user/s1962523/agg-alexa-entropy',
